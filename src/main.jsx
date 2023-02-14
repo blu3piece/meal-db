7eadc22c04f24abdcec7796df85ec730eaede259
felisZero <kcw2205@naver.com>
2023-02-14 15:59:53 +0900

    refactor: removed strict mode, index.css import

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 
 import { AppProvider } from './context';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
-  </React.StrictMode>
+  <AppProvider>
+    <App />
+  </AppProvider>
+  // React.Strictmode는 오류 검증을 위해 렌더링을 두번한다.
 )
