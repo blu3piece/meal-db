5df16208d3fdd083c9fde73c6bfd504d413b250f
felisZero <kcw2205@naver.com>
2023-02-14 15:57:12 +0900

    feat: add meal component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,9 +1,22 @@
-import { useGlobalContext } from '../context'
-
+import { useGlobalContext } from '../context';
 
 const Meals = () => {
-  const appContext = useGlobalContext();
-  return <h1>Meals / {appContext}</h1>
+  const {meals} = useGlobalContext();
+
+  return <section className='section-center'>
+    {meals.map((singleMeal) => {
+      const {idMeal, strMeal: title, strMealThumb: image } = singleMeal;
+
+      return <article key={idMeal} className="single-meal">
+        <img src={image} className="img" alt={title} />
+        <footer>
+          <h5>{title}</h5>
+          <button className='like-btn'>click me!</button>
+        </footer>
+      </article>
+
+    })}
+  </section>
 }
 
 export default Meals;
